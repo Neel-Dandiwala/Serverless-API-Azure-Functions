92c0cf7384ed5fced2d177e0a39054f64fe51a64
Neel-Dandiwala <neeldandi@gmail.com>
2021-10-27 18:29:45 +0530

    init

---
+++ api/CreateProduct/index.ts
@@ -0,0 +1,24 @@
+import { CosmosClient } from "@azure/cosmos";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import productsService from "../services/productsService";
+
+let client = new CosmosClient(process.env.CONNECTION_STRING);
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest,
+): Promise<void> {
+  let response;
+
+  try {
+    const product = req.body;
+    const result = await productsService.create(product);
+    response = { body: result, status: 200 };
+  } catch (err) {
+    response = { body: err.message, status: 500 };
+  }
+
+  context.res = response;
+};
+
+export default httpTrigger;
---
+++ api/DeleteProduct/index.ts
@@ -0,0 +1,25 @@
+import { CosmosClient } from "@azure/cosmos";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import productsService from "../services/productsService";
+
+let client = new CosmosClient(process.env.CONNECTION_STRING);
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest,
+): Promise<void> {
+  let response;
+
+  try {
+    const id = req.params.id;
+    const brand = req.body.brand;
+    const result = await productsService.delete(id, brand.name);
+    response = { body: result, status: 200 };
+  } catch (err) {
+    response = { body: err.message, status: 500 };
+  }
+
+  context.res = response;
+};
+
+export default httpTrigger;
